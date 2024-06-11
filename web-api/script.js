085f42ad462492ea8d9806c8245edbabe3b2dc59
LeafField <109466326+LeafField@users.noreply.github.com>
2024-06-11 22:54:38 +0900

    セクション2の33まで完了

--- web-api/script.js
+++ web-api/script.js
@@ -2,31 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   /** @type {HTMLDivElement}  */
   const element = document.querySelector(".square");
   const squareAnimation = element.animate(
-    // [
-    //   {
-    //     transform: "translateX(0)",
-    //     easing: "ease-in",
-    //   },
-    //   {
-    //     backgroundColor: "blue",
-    //     offset: 0.8,
-    //     composite: "replace",
-    //   },
-    //   {
-    //     transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
-    //     backgroundColor: "crimson",
-    //   },
-    // ],
-    {
-      transform: [
-        "translateX(0)",
-        "translateX(calc(100vw - 100px)) rotate(360deg)",
-      ],
-      backgroundColor: ["gold", "blue", "crimson"],
-      offset: [0, 0.3, 1],
-      easing: ["ease-in", "linear"],
-      composite: ["add", "replace", "add"],
-    },
+    [
+      {
+        transform: "translateX(0)",
+        easing: "ease-in",
+      },
+      {
+        backgroundColor: "blue",
+        offset: 0.8,
+        composite: "replace",
+      },
+      {
+        transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
+        backgroundColor: "crimson",
+      },
+    ],
     {
       duration: 3000,
       delay: 1000,
@@ -34,7 +24,50 @@ document.addEventListener("DOMContentLoaded", () => {
       fill: "both",
       iterations: Infinity,
       easing: "linear",
-      composite: "replace",
+      composite: "add",
+      iterationComposite: "accumulate",
+      timeline: document.timeline,
     }
   );
+  squareAnimation.pause();
+  console.log(squareAnimation);
+
+  /** @type {HTMLButtonElement[]} */
+  const buttons = document.querySelectorAll(".button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.classList.contains("play")) {
+        squareAnimation.play();
+      }
+      if (button.classList.contains("pause")) {
+        squareAnimation.pause();
+      }
+      if (button.classList.contains("cancel")) {
+        squareAnimation.cancel();
+      }
+      if (button.classList.contains("reverse")) {
+        squareAnimation.reverse();
+      }
+      if (button.classList.contains("finish")) {
+        squareAnimation.finish();
+      }
+    });
+
+    /** @type {HTMLInputElement} */
+    const playbackRateInput = document.getElementById("playbackRateInput");
+
+    /** @type {HTMLOutputElement} */
+    const playbackRateInputValue = document.getElementById(
+      "playbackRateInputValue"
+    );
+
+    playbackRateInput.addEventListener("input", (e) => {
+      if (!(e.target instanceof HTMLInputElement)) return;
+      squareAnimation.updatePlaybackRate(e.target.value);
+      playbackRateInputValue.value = e.target.value;
+    });
+  });
+
+  /** @type {HTMLInputElement} */
+  const durationInput = document.getElementById("durationInput");
 });
